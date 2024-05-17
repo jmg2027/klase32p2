3bfeae7b21b02b45baa4e903a55f2a3f68d45df1
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-05-17 19:43:29 +0900

    Update lsu.scala

--- src/main/scala/klase32/lsu.scala
+++ src/main/scala/klase32/lsu.scala
@@ -5,13 +5,14 @@ import chisel3.util._
 import chisel3.experimental.BundleLiterals._
 import chisel3.util.BitPat.bitPatToUInt
 import klase32.config._
-import klase32.param.KlasE32ParamKey
+import klase32.param.KLASE32ParamKey
 import snitch.enums.DataSize
 import freechips.rocketchip.rocket.constants.MemoryOpConstants
+import freechips.rocketchip.rocket.Causes
 
 
 class LSU(implicit p: Parameters) extends CoreModule with MemoryOpConstants {
-  val k = p(KlasE32ParamKey)
+  val k = p(KLASE32ParamKey)
 
   val io = IO(new Bundle{
     val lsuctrlIE = Input(new LSUControl)
@@ -23,71 +24,95 @@ class LSU(implicit p: Parameters) extends CoreModule with MemoryOpConstants {
     val rddata = Output(UInt(k.dataWidth.W))
     val wrdata = Input(UInt(k.dataWidth.W))
 
-    val rs1addr = Input(UInt(regIdWidth.W))
-    val rs2addr = Input(UInt(regIdWidth.W))
+    val storeFull = Output(Bool())
+    val loadFull = Output(Bool())
+
+    val ldXcpt = Output(new HeartXcpt)
+    val stXcpt = Output(new HeartXcpt)
   }
   )
+  // Align address
+  val addrAligned = io.addr.head(io.addr.getWidth - log2Ceil(addressAlignByte)) ## Fill(log2Ceil(addressAlignByte) - 1, 0.U)
+  val offsetAligned = io.addr(log2Ceil(addressAlignByte) - 1, 0)
 
-  // LSQ
-  // val lsq = Module(new Queue(new LoadStoreQueueIntf, loadstorequeueEntries, hasFlush = true))
-  val lsq = Vec(loadstorequeueEntries, Reg(new LoadStoreQueueIntf()))
-  val checkLoadDependency = lsq.map(x => x.isLoad && ((x.rdaddr === io.rs1addr) && (x.rdaddr === io.rs2addr)))
+  // Align store data
+  val storeDataAligned = io.wrdata << (offsetAligned << 3).asUInt
+  val storeDataMask = Mux1H(
+    Seq(
+      (io.lsuctrlIE.lsSize === DataSize.Byte) -> ("b0001".U << offsetAligned),
+      (io.lsuctrlIE.lsSize === DataSize.HalfWord) -> ("b0011".U << offsetAligned),
+      (io.lsuctrlIE.lsSize === DataSize.Word) -> "b1111".U,
+    )
+  )
 
-  // lsq stall: rd is used in previous pipeline and ack not yet reached
-  // in-order lsq, no address check
-  // check dependency only
-  // if no dependency, no stall and load/store continues to be enq
-  // lsq entry: isload, rd
-  // stalls when dependency occurs (IE/RSX, ME/RD)
-  // enq cond: !full
-  // fence -> stall pipeline until all lsq entries are committed -> empty
-  // load/store request should be issued in sequence
-  // enq and memory request
+  // Align load data
+  val loadOffsetAligned = RegEnable(offsetAligned, io.lsuctrlIE.isLoad === LoadControl.EN)
+  val loadDataAligned = {
+    val shiftedData = io.edm.ld_rdata >> (loadOffsetAligned << 3).asUInt
+    val slicedData = Mux1H(
+      Seq(
+        (io.lsuctrlME.lsSize === DataSize.Byte) -> shiftedData(7, 0),
+        (io.lsuctrlME.lsSize === DataSize.HalfWord) -> shiftedData(15, 0),
+        (io.lsuctrlME.lsSize === DataSize.Word) -> shiftedData,
+      )
+    )
+    val signedData = Wire(UInt(xLen.W))
+    signedData := Mux(io.lsuctrlME.isSigned === SignedControl.signed, slicedData.asSInt.asUInt, slicedData.asUInt)
+    signedData
+  }
+
+
+  // Store buffer
+  val sb = Module(new QueueWithAccessableEntry(new StoreBufferEntry, storeBufferEntries, flow = true))
+  sb.io.enq.valid := io.lsuctrlIE.isStore === StoreControl.EN
+  io.storeFull := sb.io.enq.ready
+  sb.io.enq.bits.addr := addrAligned
+  sb.io.enq.bits.data := storeDataAligned
+  sb.io.enq.bits.mask := storeDataMask
 
 
   // Request to DM
   io.edm.cmd := Mux1H(Seq(
     (io.lsuctrlIE.isLoad === LoadControl.EN) -> M_XRD,
-    ((io.lsuctrlIE.isStore === StoreControl.EN) &&
-      (io.lsuctrlIE.lsSize === DataSize.Word)) -> M_XWR,
-    ((io.lsuctrlIE.isStore === StoreControl.EN) &&
-      (io.lsuctrlIE.lsSize =/= DataSize.Word)) -> M_PWR,
+    ((sb.io.deq.valid) && (sb.io.deq.bits.mask === "b1111".U)) -> M_XWR,
+    ((sb.io.deq.valid) && (sb.io.deq.bits.mask =/= "b1111".U)) -> M_XWR,
   )
   )
+
   io.edm.is_mmio := DontCare
   io.edm.st_mmio_reserv := DontCare
-  // io.edm.xcpt
 
+  // Load request issues in IE
   io.edm.ld_req := io.lsuctrlIE.isLoad === LoadControl.EN
-  io.edm.ld_vaddr := io.addr
+  io.edm.ld_vaddr := addrAligned
   io.edm.ld_kill := DontCare
   io.edm.ld_mmio_kill := DontCare
+  io.loadFull := RegEnable(!io.edm.ld_ack, io.edm.ld_req)
 
-  io.edm.st_req := io.lsuctrlIE.isStore === StoreControl.EN
-  io.edm.st_paddr := io.addr
-  io.edm.st_wdata := io.wrdata
-  io.edm.st_mask := Mux1H(
-    Seq(
-      (io.lsuctrlIE.lsSize === DataSize.Byte) -> "b0001".U,
-      (io.lsuctrlIE.lsSize === DataSize.HalfWord) -> "b0011".U,
-      (io.lsuctrlIE.lsSize === DataSize.Word) -> "b1111".U,
-    )
-  )
+  // Store request issues from store buffer
+  io.edm.st_req := sb.io.deq.valid
+  sb.io.deq.ready := io.edm.st_ack
+  io.edm.st_paddr := sb.io.deq.bits.addr
+  io.edm.st_wdata := sb.io.deq.bits.data
+  io.edm.st_mask := sb.io.deq.bits.mask
   io.edm.st_mmio := DontCare
-  // ld_ack, st_ack
 
   // Response from DM: ME stage
-  io.rddata := Mux1H(
-    Seq(
-      ((io.lsuctrlME.lsSize === DataSize.Byte) &&
-        (io.lsuctrlME.isSigned === SignedControl.signed)) -> Cat(Fill(k.dataWidth - 7, 0.U), io.edm.ld_rdata(7, 0)),
-      ((io.lsuctrlME.lsSize === DataSize.Byte) &&
-        (io.lsuctrlME.isSigned === SignedControl.unsigned)) -> io.edm.ld_rdata(7, 0).zext.asUInt,
-      ((io.lsuctrlME.lsSize === DataSize.HalfWord) &&
-        (io.lsuctrlME.isSigned === SignedControl.signed)) -> Cat(Fill(k.dataWidth - 15, 0.U), io.edm.ld_rdata(15, 0)),
-      ((io.lsuctrlME.lsSize === DataSize.HalfWord) &&
-        (io.lsuctrlME.isSigned === SignedControl.unsigned)) -> io.edm.ld_rdata(15, 0).zext.asUInt,
-      (io.lsuctrlME.lsSize === DataSize.Word) -> io.edm.ld_rdata,
-    )
-  )
+  io.rddata := Mux(io.edm.ld_ack, loadDataAligned, 0.U)
+
+  // Exception
+  io.stXcpt := {
+    val result = Wire(new HeartXcpt)
+    (result.getElements zip io.edm.xcpt.getElements).foreach {
+      case (a, b) => a.asInstanceOf[Bool] := b.asInstanceOf[Bool] && io.edm.st_ack
+    }
+    result
+  }
+  io.ldXcpt := {
+    val result = Wire(new HeartXcpt)
+    (result.getElements zip io.edm.xcpt.getElements).foreach {
+      case (a, b) => a.asInstanceOf[Bool] := b.asInstanceOf[Bool] && io.edm.ld_ack
+    }
+    result
+  }
 }
