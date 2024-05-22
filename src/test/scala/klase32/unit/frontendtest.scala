415b11e07124d5bc43b8c633062caf0f61579632
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-05-22 16:34:31 +0900

    Update frontendtest.scala

--- src/test/scala/klase32/unit/frontendtest.scala
+++ src/test/scala/klase32/unit/frontendtest.scala
@@ -1,20 +1,19 @@
-package klase32.unit
+package klase32
 
 import chisel3._
 import chiseltest._
-import org.scalatest.flatspec.AnyFlatSpec
+import klas.KlasTest
 import klase32.config._
-import klase32.param.KlasE32ParamKey
+import klase32.param.DefaultConfig
 
-class FrontendTest extends AnyFlatSpec with ChiselScalatestTester {
+class FrontendTest extends KlasTest {
+  implicit val p = new DefaultConfig
   behavior of "Frontend"
 
   it should "correctly handle control flows and fetch operations" in {
-    implicit val p: Parameters = new DefaultConfig() // Ensure this matches your configuration class
-
     test(new Frontend) { c =>
       // Function to simulate a single clock cycle of operation
-      def simulateCycle(ctrlSignal: CtrlControlIE.Type, pc: BigInt, evec: BigInt, cnd: Boolean, exception: Boolean, eret: Boolean, stall: Boolean, divBusy: Boolean, aluR: BigInt): Unit = {
+      def simulateCycle(ctrlSignal: FrontendControlIE.Type, pc: BigInt, evec: BigInt, cnd: Boolean, exception: Boolean, eret: Boolean, stall: Boolean, divBusy: Boolean, aluR: BigInt): Unit = {
         c.io.ctrl.poke(ctrlSignal)
         c.io.if_pc.poke(pc.U)
         c.io.evec.poke(evec.U)
@@ -25,24 +24,37 @@ class FrontendTest extends AnyFlatSpec with ChiselScalatestTester {
         c.io.divBusy.poke(divBusy.B)
         c.io.aluR.poke(aluR.U)
         c.clock.step(1) // Advance the clock to process the inputs
+        println(c.io.issue.peekBoolean())
+        println(c.io.pcRegWrite.valid.peekBoolean())
+        println(c.io.pcRegWrite.bits.peekInt())
+        println(c.io.epm.elements.map {
+          case (k, v) =>
+            k -> v.peek()
+//            v match {
+//            case _: UInt => v.asUInt.peek()
+//            case _: Bool => v.asBool.peek()
+//            case _ => v
+//          }
+        }
+        )
       }
 
       // Initial Conditions
       c.reset.poke(true.B)
-      simulateCycle(CtrlControlIE.NOOP, 0x1000, 0x0, cnd = false, exception = false, eret = false, stall = false, divBusy = false, 0x0)
+      simulateCycle(FrontendControlIE.default, 0x1000, 0x0, cnd = false, exception = false, eret = false, stall = false, divBusy = false, 0x0)
       c.reset.poke(false.B)
 
       // Test basic fetch
-      simulateCycle(CtrlControlIE.NOOP, 0x1000, 0x0, cnd = false, exception = false, eret = false, stall = false, divBusy = false, 0x0)
+      simulateCycle(FrontendControlIE.default, 0x1000, 0x0, cnd = false, exception = false, eret = false, stall = false, divBusy = false, 0x0)
 
       // Test branching
-      simulateCycle(CtrlControlIE.BR, 0x1000, 0x0, cnd = true, exception = false, eret = false, stall = false, divBusy = false, 0x1040)
+      simulateCycle(FrontendControlIE.BR, 0x1000, 0x0, cnd = true, exception = false, eret = false, stall = false, divBusy = false, 0x1040)
 
       // Test stall conditions
-      simulateCycle(CtrlControlIE.NOOP, 0x1040, 0x0, cnd = false, exception = false, eret = false, stall = true, divBusy = false, 0x1040)
+      simulateCycle(FrontendControlIE.default, 0x1040, 0x0, cnd = false, exception = false, eret = false, stall = true, divBusy = false, 0x1040)
 
       // Test exception handling
-      simulateCycle(CtrlControlIE.NOOP, 0x1040, 0x2000, cnd = false, exception = true, eret = false, stall = false, divBusy = false, 0x1040)
+      simulateCycle(FrontendControlIE.default, 0x1040, 0x2000, cnd = false, exception = true, eret = false, stall = false, divBusy = false, 0x1040)
 
       // Additional scenarios to cover jumps, eret, flushes, and interrupts
     }
