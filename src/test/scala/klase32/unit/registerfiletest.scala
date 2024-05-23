48b9bf9090b2fcd753b3b0496350d5f7d7cde47b
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-05-23 10:51:15 +0900

    Update registerfiletest.scala

--- src/test/scala/klase32/unit/registerfiletest.scala
+++ src/test/scala/klase32/unit/registerfiletest.scala
@@ -4,35 +4,36 @@ import chisel3._
 import chiseltest._
 import klas.KlasTest
 import klase32.config._
-import klase32.param.DefaultConfig
+import klase32.param.{DefaultConfig, KLASE32ParamKey}
 
 class RegisterFileTest extends KlasTest {
   behavior of "RegisterFile"
 
   it should "correctly handle read and write operations" in {
     implicit val p: Parameters = new DefaultConfig() // Ensure this matches your configuration class
+    val k = p(KLASE32ParamKey)
 
-    test(new RegisterFile) { rf =>
+    test(new RegisterFile) { dut =>
       // Helper function to perform a write
       def writeRegister(index: Int, data: BigInt): Unit = {
-        rf.io.wp(index).valid.poke(true.B)
-        rf.io.wp(index).bits.addr.poke(index.U)
-        rf.io.wp(index).bits.data.poke(data.U)
-        rf.clock.step(1) // Advance the clock to process the write
-        rf.io.wp(index).valid.poke(false.B) // Deassert write valid
+        dut.io.wp(index).valid.poke(true.B)
+        dut.io.wp(index).bits.addr.poke(index.U)
+        dut.io.wp(index).bits.data.poke(data.U)
+        dut.clock.step(1) // Advance the clock to process the write
+        dut.io.wp(index).valid.poke(false.B) // Deassert write valid
       }
 
       // Helper function to read and check a register
       def readAndCheck(index: Int, expectedData: BigInt): Unit = {
-        rf.io.rp(index).addr.poke(index.U)
-        rf.clock.step(1) // Advance the clock to ensure read occurs
-        assert(rf.io.rp(index).data.peek().litValue == expectedData, s"Register $index did not have expected value $expectedData")
+        dut.io.rp(index).addr.poke(index.U)
+        dut.clock.step(1) // Advance the clock to ensure read occurs
+        assert(dut.io.rp(index).data.peek().litValue == expectedData, s"Register $index did not have expected value $expectedData")
       }
 
       // Reset the register file
-      rf.reset.poke(true.B)
-      rf.clock.step(1)
-      rf.reset.poke(false.B)
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
 
       // Test Writing to and Reading from registers
       writeRegister(0, 0x12345678L)
@@ -48,7 +49,7 @@ class RegisterFileTest extends KlasTest {
       readAndCheck(0, 0)
 
       // Test all registers
-      for (addr <- 1 until regNum) {
+      for (addr <- 1 until k.core.regNum) {
         val testData = addr * 2
         writeRegister(addr, testData)
         readAndCheck(addr, testData)
