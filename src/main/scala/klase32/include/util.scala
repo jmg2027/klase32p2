3fb93de90856a3d22e4c910bda06e5acd6841bb5
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-05-23 14:44:09 +0900

    Create util.scala

---
+++ src/main/scala/klase32/include/util.scala
@@ -0,0 +1,31 @@
+package klase32
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+import klase32.config._
+import chisel3.util.log2Ceil
+import klase32.param.KLASE32ParamKey
+import scala.language.experimental.macros
+
+class QueueWithAccessableEntry[T <: Data](
+   override val gen:            T,
+   override val entries:        Int,
+   override val pipe:           Boolean = false,
+   override val flow:           Boolean = false,
+   override val useSyncReadMem: Boolean = false,
+   override val hasFlush:       Boolean = false
+  )(
+   implicit compileOptions: chisel3.CompileOptions)
+extends Queue[T](gen, entries, pipe, flow, useSyncReadMem, hasFlush) {
+  val entry = IO(Output(Vec(entries, genType)))
+  for (i <-0 until entries) {
+    entry(i) := ram(i)
+  }
+}
+
+object util {
+  def connectBundle[T <: Record](abundle: T, bbundle: T) = {
+
+  }
+}
