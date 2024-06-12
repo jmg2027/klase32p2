2164a62d45a108f0d6a4fb4622aaf7a5652d8008
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-06-12 17:54:22 +0900

    Update frontend.scala

--- src/main/scala/klase32/frontend.scala
+++ src/main/scala/klase32/frontend.scala
@@ -2,262 +2,276 @@ package klase32
 
 import chisel3._
 import chisel3.util._
-import chisel3.experimental.BundleLiterals._
+import chisel3.util.BitPat.bitPatToUInt
 import klase32.config._
-import klase32.param.KLASE32ParamKey
+import snitch.enums.{OperandType, RdType}
+import freechips.rocketchip.rocket.Causes
 
-class FrontendIO(implicit p: Parameters) extends CoreBundle with HasCoreParameters {
-  val ctrl = Input(FrontendControlIE())
-
-  val if_pc = Output(UInt(mxLen.W)) // Current PC
-  val evec = Input(UInt(mxLen.W))
-  val cnd = Input(Bool())
-  val exception = Input(Bool())
-  val eret = Input(Bool())
-
-  val divBusy = Input(Bool())
-  val stall = Input(Bool())
-  val wfi = Input(Bool())
-
-  val aluR = Input(UInt(mxLen.W))
-  val ie_pc = Input(UInt(mxLen.W))
-  val brOffset = Input(UInt(13.W))
-
-  val issue = Output(Bool())
-
-  val instPacket = Output(new Bundle {
-    val inst = UInt(wordsize.W)
-    val xcpt = new HeartXcpt
-  }) // IF stage
-
-  val flushFetchQueue = Input(new Flush())
-  val flushIcache = Input(IcacheFlushIE())
-  val jump = Output(Bool())
-
+class KLASE32IO(implicit p: Parameters) extends Bundle with KLASE32IOEtc {
+  //  val acc = new Acc.Interface
+  val interrupt = Input(new Interrupt)
+  val edm = new EdmIntf
   val epm = new EpmIntf
+  //  val jtag = new JtagIntf
+  //  val dbg = new DbgIntf
 }
 
+trait KLASE32IOEtc extends Bundle {
+  val powerdown = Output(Bool())
+}
 
-class Frontend(implicit p: Parameters) extends CoreModule {
+class KLASE32(hartId: Int)(implicit p: Parameters) extends CoreModule
+  with HasCoreParameters {
+  val NOP = BitPat("b00000000000000000000000000010011")
 
-  import FrontendControlIE._
+  val io = IO(new KLASE32IO)
 
-  val k = p(KLASE32ParamKey)
 
-  val io = IO(new FrontendIO())
+  // Module definition
+  val alu = Module(new ALU)
+  val lsu = Module(new LSU)
+  //val div = Module(new DIV)
+  //val mpy = Module(new MPY)
+  val frontend = Module(new Frontend)
+  val csr = Module(new CSRModule)
+  val hzd = Module(new Hazards)
+  //val dbgCtrl = Module(new DebugController)
+  //val jtag = Module(new JtagInterface)
+  val dec = Module(new Decoder)
+  val reg = Module(new RegisterFile())
 
+
+  // control signal
+  val ctrlSig = dec.io.decSig
+
+  // Stall
+  val stallSig = WireInit(new Stall, DontCare)
+  stallSig.ie.issue := !frontend.io.issue
+  stallSig.ie.store := lsu.io.storeFull
+  // stallSig.ie.csr := csr.io.csrWrite
+  stallSig.me.load := lsu.io.loadFull
+  stallSig.me.wfi := csr.io.wfiOut
+  stallSig.me.hzd := hzd.io.stall
+  stallSig.me.fence := ctrlSig.fence.asUInt.orR && (lsu.io.loadFull || lsu.io.storeFull)
+  val stallIE = stallSig.ie.orR
+  val stallME = stallSig.me.orR
+  val stall = stallIE || stallME
+
+  // Flush
+  // FIXME: Can be optimized
+  val flushSig = WireInit(new Flush, DontCare)
+  flushSig.ie.jump := frontend.io.jump
+  // flushSig.ie.xcpt := meXcpt
+  flushSig.ie.xcpt := frontend.io.exception
+  flushSig.ie.eret := ctrlSig.ecall.asUInt.orR || ctrlSig.ebreak.asUInt.orR || ctrlSig.mret.asUInt.orR
+  flushSig.ie.csr := csr.io.csrWrite
+  flushSig.ie.fence := ctrlSig.fence.asUInt.orR && (lsu.io.loadFull || lsu.io.storeFull)
+  val flushIE = flushSig.ie.orR
+  val flush = reset.asBool || flushIE
+
+  // Pipeline
+  //  val ie_inst = RegEnable(frontend.io.instPacket.inst, bitPatToUInt(NOP), !stall && frontend.io.issue)
+  val ie_inst = withReset(flush) {RegEnable(frontend.io.instPacket.bits.data, bitPatToUInt(NOP), !stall)}
+  val ie_pc = withReset(flush) {RegEnable(frontend.io.if_pc, !stall)}
+
+  printf(cf"===============================New Cycle===============================\n")
+  // printf(cf"ie_inst: ${ie_inst}%x\n")
+  // printf(cf"ie_pc: ${ie_pc}%x\n")
+  // printf(cf"ctrlSig: ${ctrlSig}\n")
+  // printf(cf"stallSig.ie.issue: ${stallSig.ie.issue}\n")
+  // printf(cf"stallSig.ie.store: ${stallSig.ie.store}\n")
+  // printf(cf"stallSig.ie.csr: ${stallSig.ie.csr}\n")
+  // printf(cf"stallSig.me.load: ${stallSig.me.load}\n")
+  // printf(cf"stallSig.me.wfi: ${stallSig.me.wfi}\n")
+  // printf(cf"stallSig.me.hzd: ${stallSig.me.hzd}\n")
+  // printf(cf"stallSig.me.fence: ${stallSig.me.fence}\n")
+
+  //  val me_inst = RegEnable(ie_inst, bitPatToUInt(NOP), !stallME)
+  val me_pc = withReset(flush) {RegEnable(ie_pc, !stallME)}
+  val me_lsu = withReset(flush) {RegEnable(ctrlSig.lsuCtrl, !stallME)}
+  val me_isLoad = withReset(flush) {RegEnable(ctrlSig.lsuCtrl.isLoad, !stallME)}
+  val me_rdaddr = withReset(flush) {RegEnable(ctrlSig.rd, !stallME)}
+  val me_aluR = withReset(flush) {RegEnable(alu.io.R, !stallME)}
+
+
+  // Exception
+  val (ieXcpt, ieCause): (Bool, UInt)  = checkExceptions(List(
+    (csr.io.interruptPending, csr.io.interruptCause),
+    (frontend.io.instPacket.bits.xcpt.ma, Causes.misaligned_fetch.U),
+    (frontend.io.instPacket.bits.xcpt.pf, Causes.fetch_page_fault.U),
+    (frontend.io.instPacket.bits.xcpt.gf, Causes.fetch_guest_page_fault.U),
+    (frontend.io.instPacket.bits.xcpt.ae, Causes.fetch_access.U),
+
+    (lsu.io.stXcpt.ma, Causes.misaligned_store.U),
+    (lsu.io.stXcpt.pf, Causes.store_page_fault.U),
+    (lsu.io.stXcpt.gf, Causes.store_guest_page_fault.U),
+    (lsu.io.stXcpt.ae, Causes.store_access.U),
+    (ctrlSig.illegal === IllegalInstIE.illegal, Causes.illegal_instruction.U),
+    (ctrlSig.ecall === EcallIE.EN, Causes.machine_ecall.U),
+    (ctrlSig.ebreak === EbreakIE.EN, Causes.breakpoint.U),
+
+    // CHECK: No need to pipeline for load exception...
+    (lsu.io.ldXcpt.ma, Causes.misaligned_load.U),
+    (lsu.io.ldXcpt.pf, Causes.load_page_fault.U),
+    (lsu.io.ldXcpt.gf, Causes.load_guest_page_fault.U),
+    (lsu.io.ldXcpt.ae, Causes.load_access.U),
+  ))
+//
+//  // Exception pipeline
+//  val ieXcptReg = RegEnable(ieXcpt, !stallME)
+//  val ieCauseReg = RegEnable(ieCause, !stallME)
+//
+//  // FIXME: Until cache
+//  val (meXcpt, meCause): (Bool, UInt)= checkExceptions(List(
+//    (ieXcptReg, ieCauseReg),
+//    (lsu.io.ldXcpt.ma, Causes.misaligned_load.U),
+//    (lsu.io.ldXcpt.pf, Causes.load_page_fault.U),
+//    (lsu.io.ldXcpt.gf, Causes.load_guest_page_fault.U),
+//    (lsu.io.ldXcpt.ae, Causes.load_access.U),
+//  ))
+
+
+  // Fetch & Issue
+  io.epm <> frontend.io.epm
   io.epm.kill := DontCare
 
-  val brCond = (io.ctrl === BR) & io.cnd
-  val haltCond = (io.ctrl === HALT)
-  val jalCond = (io.ctrl === JAL)
-  val jalrCond = (io.ctrl === JALR) | (io.ctrl === MRET)
 
-  // After n cycle fetch will start
-  val n = 1 // booting cycle
-  val bootingCounter = RegInit(0.U(log2Ceil(n).W))
-  val regBooting = RegInit(true.B)
-  when(reset.asBool) {
-    regBooting := true.B
-    bootingCounter := 0.U
-  }.elsewhen(regBooting && bootingCounter === (n - 1).U) {
-    regBooting := false.B
-  }.elsewhen(regBooting) {
-    bootingCounter := bootingCounter + 1.U
-  }
+  // ctrl
+  frontend.io.ctrl := ctrlSig.frontendCtrl
+  frontend.io.flushIcache := ctrlSig.flushICache
+  frontend.io.flushFetchQueue := flushSig ; dontTouch(frontend.io.flushFetchQueue)
 
-  val bootAddrWire = WireDefault(bootAddrParam.U)
-  if (usingOuterBoodAddr) {
-    bootAddrWire := io.epm.bootAddr
-  }
+  frontend.io.divBusy := DontCare
+  frontend.io.wfi := csr.io.wfiOut
 
-  // Next PC
-  val brIE = brCond && !io.stall
-  val jalIE = jalCond && !io.stall
-  val jalrIE = jalrCond && !io.stall
-  //  val jump = brIE || jalIE || jalrIE || regBooting
-  val jump = brIE || jalIE || jalrIE
-  io.jump := jump
+  frontend.io.evec := csr.io.evec
+  frontend.io.cnd := alu.io.F
+  // FIXME: Should handle this
+  // frontend.io.exception := meXcpt
+  frontend.io.exception := ieXcpt
+  frontend.io.eret := ctrlSig.ecall.asUInt.orR || ctrlSig.ebreak.asUInt.orR || ctrlSig.mret.asUInt.orR
 
-  val brAddr = io.ie_pc + io.brOffset
-  val jumpPC = Mux(brIE, brAddr, io.aluR)
+  frontend.io.stall := stall
 
-  // Fetch Queue
-  // FIXME: Support for C extension
-  // For now 32-bit N entries
-  // To extend to compressed mode, fetch queue should handle 16-bit data
-  // when empty, enq data will be dequeued instantly
-  // FIXME: Check for flow in jump
-  //   val fq = Module(new Queue(new FetchQueueEntry, fetchqueueEntries, flow = true, hasFlush = true))
-  val fq = Module(new Queue(new FetchQueueEntry, fetchqueueEntries, flow = false, hasFlush = true))
+  frontend.io.aluR := alu.io.R
+  frontend.io.brOffset := ctrlSig.imm.b.asUInt
+  frontend.io.ie_pc := ie_pc
 
-  // Fetch
-  // Fetch counter
-  val fqCounter = withReset(reset.asBool || fq.flush) { RegInit((fetchqueueEntries - 1).U) }
-
-  when (fq.io.enq.fire && fq.io.deq.fire) {
-    fqCounter := fqCounter
-  }.elsewhen (fq.io.enq.fire) {
-    fqCounter := fqCounter - 1.U
-  }.elsewhen (fq.io.deq.fire) {
-    fqCounter := fqCounter + 1.U
-  }
-  val fetchAvail = fqCounter =/= 0.U
-
-  // When exception happens, only 1 request can go out
-  val xcptReqOntheway = RegInit(false.B)
-  when (io.epm.req && io.exception && !regBooting) {
-    xcptReqOntheway := true.B
-  }
-  when(io.epm.ack && xcptReqOntheway) {
-    xcptReqOntheway := false.B
-  }
-
-  // Flush fetch queue
-  fq.flush := io.flushFetchQueue.ie.xcpt ||
-    io.flushFetchQueue.ie.jump ||
-    io.flushFetchQueue.ie.eret ||
-    io.flushFetchQueue.ie.csr
-  // Fence does not flush fetch queue
-  // fq.flush := io.flushFetchQueue.orR
-
-  val fetch = fetchAvail && !regBooting && !xcptReqOntheway && !io.wfi
-  val fetchPC = Reg(UInt(mxLen.W))
-
-  printf(cf"[FE] exception: ${io.exception}\n")
-  printf(cf"[FE] evec: ${io.evec}%x\n")
-  when(regBooting) {
-    fetchPC := bootAddrWire
-  }.elsewhen(io.exception || io.eret) {
-    fetchPC := io.evec
-  }.elsewhen(jump) {
-    fetchPC := jumpPC
-  }.elsewhen(io.flushFetchQueue.ie.csr.orR) {
-    fetchPC := io.ie_pc + 4.U
-  }.elsewhen(fetch) {
-    fetchPC := fetchPC + 4.U
-  }.otherwise {
-    fetchPC := fetchPC
-  }
-  //}.elsewhen(!fetch) {
-  ////    fetch := false.B
-  //    fetchPC := fetchPC
-  //    fq.flush := false.B
-  //    // FIXME: Does this not needed?
-  //  }.otherwise {
-  ////    fetch := fq.io.enq.ready
-  ////    fetch := true.B
-  //    fetchPC := fetchPC + 4.U
-  //    fq.flush := false.B
-  //  }
-
-  // When flush, ignore acks for on-the-fly reqs
-  // All requests recieve ack so overflow should not happen
-  // Flush can occur only after when enq.valid is enable, except for async exception
-  // So maximum number of on-the-fly requests are 2 times of fetchqueueEntries
-  // For exception(async exception), only 1 request will comes out
-  val reqCounter = RegInit(0.U(log2Ceil(fetchqueueEntries * 2 - 1).W))
-  val ignoreAck = RegInit(false.B)
-//  val ignoreAckWire = RegInit(false.B)
-//  when(io.epm.req && io.epm.ack) {
-//    reqCounter := reqCounter
-//  }.elsewhen(io.epm.req) {
-//    reqCounter := reqCounter + 1.U
-//  }.elsewhen(io.epm.ack) {
-//    reqCounter := reqCounter - 1.U
-//  }
-//
-//  ignoreAckWire := fq.flush && ignoreAck
-//  when(fq.flush) {
-//    ignoreAck := ignoreAckWire
-//  }.elsewhen(ignoreAck) {
-//    ignoreAck := reqCounter =/= 1.U
-//  }
+  // Decode
+  dec.io.inst := ie_inst
 
 
-  when(fq.flush && !xcptReqOntheway) {
-    reqCounter := reqCounter + (fetchqueueEntries - 1).U - fqCounter
-    ignoreAck := true.B
-  }
+  // Register file
+  reg.io.rp(0).addr := ctrlSig.rs1
+  reg.io.rp(1).addr := ctrlSig.rs2
+  // FIXME: Why this prints 0??
+  val rs1 = Mux(!hzd.io.bypassRS1RD, reg.io.rp(0).data, lsu.io.rddata)
+  //  val rs1 = reg.io.rp(0).data
+  // printf(cf"reg.io.rp(0).addr: ${reg.io.rp(0).addr}\n")
+  // printf(cf"reg.io.rp(0).data: ${reg.io.rp(0).data}%x\n")
+  // printf(cf"reg.io.rp(1).addr: ${reg.io.rp(1).addr}\n")
+  // printf(cf"reg.io.rp(1).data: ${reg.io.rp(1).data}%x\n")
+  val rs2 = Mux(!hzd.io.bypassRS2RD, reg.io.rp(1).data, lsu.io.rddata)
+  //  val rs2 = reg.io.rp(1).data
 
-  when(ignoreAck && io.epm.ack) {
-    reqCounter := reqCounter - 1.U
-  }
+  reg.io.wp(0).bits.addr := ctrlSig.rd
+  reg.io.wp(1).bits.addr := me_rdaddr
+  reg.io.wp(0).valid := ctrlSig.w0Wb.asUInt.orR && !stall
+  //  reg.io.wp(0).valid := ctrlSig.w0Wb.asUInt.orR
+  // FIXME: Decoupling
+  reg.io.wp(1).valid := me_isLoad.asUInt.orR && !stallME
+  //  reg.io.wp(1).valid := me_isLoad.asUInt.orR
+  // printf(cf"reg.io.wp(0).valid: ${reg.io.wp(0).valid}\n")
+  // printf(cf"alu.io.R: ${alu.io.R}%x\n")
 
-  //  when(reqCounter === 0.U && ignoreAck) {
-  when(reqCounter === 1.U && ignoreAck && io.epm.ack) {
-    ignoreAck := false.B
-  }
-
-  io.epm.addr := fetchPC
-  // printf(cf"[FE] fetchPC: 0x${io.epm.addr}%x\n")
-  //  io.epm.req := fetch
-  io.epm.req := fetch
-  // FIXME: Should be handled in the future
-  io.epm.flush := io.flushIcache.asUInt.orR
-  // io.epm.flush := io.flushIcache.asUInt.orR || io.flushFetchQueue.ie.csr
-  io.epm.cmd := 0.U // int load
-
-  fq.io.enq.bits.data := io.epm.data
-  fq.io.enq.bits.xcpt := io.epm.xcpt
-//  fq.io.enq.valid := io.epm.ack && !ignoreAck && fetchAvail// Suppose simultaneous ack and data response
-  fq.io.enq.valid := io.epm.ack && !ignoreAck && fetchAvail// Suppose simultaneous ack and data response
-
-
-  // Why this generates nullpointerexception?
-  //  def isRVC(inst: UInt): Bool = !(inst(1) && inst(0))
-
-  // Issue
-  val issueable = !io.divBusy
-  /* FIXME: RVC
-  Fetch queue will fetch fetchwidth. fetchqueue should store 2 bytes align
-  Extended instruction will be issued: 32 bits
-   */
-//  val instrAvail = fq.io.deq.valid && !regBooting && !ignoreAckWire && !ignoreAck
-  val instrAvail = fq.io.deq.valid && !regBooting
-  // When fq being flushed, NOP is issued(1 cycle stall)
-  val issue = instrAvail && issueable && !ignoreAck
-
-  val instIF = fq.io.deq.bits.data
-  val xcptIF = fq.io.deq.bits.xcpt
+  reg.io.wp(0).bits.data := Mux1H(Seq(
+    RdType.Alu -> alu.io.R,
+    //    RdType.ConsecPC -> (frontend.io.if_pc + 4.U),
+    //    RdType.ConsecPC -> (frontend.io.if_pc),
+    RdType.ConsecPC -> (frontend.io.ie_pc + 4.U),
+    RdType.BypassCSR -> csr.io.rd
+  ).map {case(k, v) => (k === ctrlSig.rdType, v)})
+  reg.io.wp(1).bits.data := lsu.io.rddata
 
 
-//  val pcWrite = Wire(UInt())
-//  val pcWrite = RegInit(bootAddrWire)
+  // ALU
+  alu.io.ctrl := ctrlSig.aluCtrl
 
-  // PC Register
-  // Address of current instruction in IF stage
-  // val pcReg = RegEnable(pcWrite, bootAddrWire, (jump || issue) && !io.stall)
-//  val nextPC = RegInit(bootAddrWire)
-//  val pcReg = RegEnable(nextPC, bootAddrWire, issue)
-  val pcReg = RegInit(bootAddrWire)
-  io.if_pc := pcReg
-  printf(cf"[FE] pc: 0x$pcReg%x\n")
-  // printf(cf"[FE] jump: $jump\n")
-  // printf(cf"[FE] issue: $issue\n")
-  // printf(cf"boot: $regBooting\n")
-  // printf(cf"fq: ${fq.io.deq.valid}\n")
-  // printf(cf"stall: ${io.stall}\n")
+  alu.io.A := Mux1H(Seq(
+    OperandType.None -> 0.U,
+    OperandType.Reg -> rs1,
+    OperandType.PC -> ie_pc,
+    OperandType.CSRImmediate -> ctrlSig.rs1 // rs1 field of instruction is imm field
+  ).map { case (k, v) => (k === ctrlSig.operandSelect.a, v) })
+
+  alu.io.B := Mux1H(Seq(
+    OperandType.None -> 0.U,
+    OperandType.Reg -> rs2,
+    OperandType.IImmediate -> ctrlSig.imm.i.asUInt,
+    OperandType.UImmediate -> ctrlSig.imm.u.asUInt,
+    OperandType.JImmediate -> ctrlSig.imm.j.asUInt,
+    OperandType.SImmediate -> ctrlSig.imm.s.asUInt
+  ).map { case (k, v) => (k === ctrlSig.operandSelect.b, v) })
+  // printf(cf"ctrlSig.operandSelect.a: ${ctrlSig.operandSelect.a}\n")
+  // printf(cf"ctrlSig.operandSelect.b: ${ctrlSig.operandSelect.b}\n")
+  // printf(cf"alu.io.ctrl: ${alu.io.ctrl}\n")
+  // printf(cf"alu.io.A: ${alu.io.A}%x\n")
+  // printf(cf"alu.io.B: ${alu.io.B}%x\n")
 
 
 
-  when(issue && !io.stall) {
-    when(io.exception || io.eret) {
-      pcReg := io.evec
-    }.elsewhen(jump) {
-      pcReg := jumpPC
-    }.elsewhen(io.flushFetchQueue.ie.csr.orR) {
-      pcReg := io.ie_pc + 4.U
-    }.otherwise {
-      pcReg := pcReg + 4.U
-    }
-  }
-  // pcWrite := Mux(jump, jumpPC, pcReg + 4.U)
-//  pcWrite := Mux(fq.flush, fetchPC, pcReg + 4.U)
+  // CSR
+  csr.io.ctrl.inst := ctrlSig.csrCtrl
+  csr.io.ctrl.in := alu.io.R
+  csr.io.ctrl.addr := ie_inst(31, 20)
 
-  // Issue
-  fq.io.deq.ready := !io.stall // issue signal will block when stalled
-  io.instPacket.inst := instIF
-  io.instPacket.xcpt := xcptIF
-  io.issue := issue
+  csr.io.hartId := hartId.U
+  csr.io.ecall := ctrlSig.ecall
+  csr.io.ebreak := ctrlSig.ebreak
+  csr.io.mret := ctrlSig.mret
+  csr.io.wfi := ctrlSig.wfi
+
+  // FIXME: When speculative load/store supported, exception should be failed load/store pc
+//  csr.io.exception := meXcpt
+//  csr.io.cause := meCause
+  csr.io.exception := ieXcpt
+  csr.io.cause := ieCause
+  csr.io.epc := me_pc
+  csr.io.loadAddr := me_aluR
+
+  io.powerdown := csr.io.wfiOut
+
+  // interrupt
+  csr.io.interrupt := io.interrupt
+
+
+  // Hazard
+  // FIXME: Why does this not work?
+  hzd.io.rs1Valid := ((OperandType.Reg) === (ctrlSig.operandSelect.a))
+  // RS2: ALU.B || Store || Branch
+  hzd.io.rs2Valid := ((ctrlSig.operandSelect.b) === (OperandType.Reg)) ||
+    ((ctrlSig.frontendCtrl) === (FrontendControlIE.BR)) ||
+    ((ctrlSig.lsuCtrl.isStore) === (StoreControl.EN))
+  hzd.io.loadValid := me_isLoad === LoadControl.EN
+
+  hzd.io.rs1Addr := ctrlSig.rs1
+  hzd.io.rs2Addr := ctrlSig.rs2
+  hzd.io.rdAddrME := me_rdaddr
+  hzd.io.rdAddrIE := ctrlSig.rd
+  lsu.io.ldKill := hzd.io.ldKill
+
+  // LSU
+  lsu.io.lsuctrlIE <> ctrlSig.lsuCtrl
+  lsu.io.lsuctrlME <> me_lsu
+
+  lsu.io.edm <> io.edm
+  lsu.io.addr := alu.io.R
+  lsu.io.wrdata := rs2
+  // printf(cf"lsu.io.addr: ${lsu.io.addr}%x\n")
+  // printf(cf"lsu.io.wrdata: ${lsu.io.wrdata}%x\n")
+  lsu.io.stall := stall
+  lsu.io.stallME := stallME
+
+  def checkExceptions(x: Seq[(Bool, UInt)]) =
+    (x.map(_._1).reduce(_||_), PriorityMux(x))
 }
