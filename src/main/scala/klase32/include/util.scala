8cac6e012fbbbfcf5f3b282aee45c617f69a151c
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2024-08-01 15:21:13 +0900

    Update util.scala

--- src/main/scala/klase32/include/util.scala
+++ src/main/scala/klase32/include/util.scala
@@ -8,15 +8,15 @@ import chisel3.util.log2Ceil
 import scala.language.experimental.macros
 
 class QueueWithAccessableEntry[T <: Data](
-   override val gen:            T,
-   override val entries:        Int,
-   override val pipe:           Boolean = false,
-   override val flow:           Boolean = false,
-   override val useSyncReadMem: Boolean = false,
-   override val hasFlush:       Boolean = false
-  )(
-   implicit compileOptions: chisel3.CompileOptions)
-extends Queue[T](gen, entries, pipe, flow, useSyncReadMem, hasFlush) {
+                                           override val gen:            T,
+                                           override val entries:        Int,
+                                           override val pipe:           Boolean = false,
+                                           override val flow:           Boolean = false,
+                                           override val useSyncReadMem: Boolean = false,
+                                           override val hasFlush:       Boolean = false
+                                         )(
+                                           implicit compileOptions: chisel3.CompileOptions)
+  extends Queue[T](gen, entries, pipe, flow, useSyncReadMem, hasFlush) {
   val entry = IO(Output(Vec(entries, genType)))
   for (i <-0 until entries) {
     entry(i) := ram(i)
