8d9702c17d95c463a29d81a466981c0118f0bd84
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2025-04-22 21:32:17 +0900

    Update mpy.scala

--- src/main/scala/klase32/mpy.scala
+++ src/main/scala/klase32/mpy.scala
@@ -3,8 +3,10 @@ package klase32
 import chisel3._
 import chisel3.util._
 import chisel3.experimental.BundleLiterals._
-import klase32.config._
-import klase32.param.KLASE32ParamKey
+import klase32.include.config._
+import klase32.include.param.KLASE32ParamKey
+import klase32.include.KLASE32AbstractClass._
+import klase32.include.ControlSignal._
 // import klase32.interface._
 
 
@@ -28,10 +30,6 @@ class MPY(implicit p: Parameters) extends CoreModule {
   val rs1Signed = Wire(Bool())
   val rs2Signed = Wire(Bool())
   val mostSignificantWord = Wire(Bool())
-
-  // State machine
-  val sIdle :: sCompute :: sDone :: Nil = Enum(3)
-  val state = RegInit(sIdle)
 
   //   Assume control signal does not fall down
   //    val valid = io.ctrl =/= default
@@ -75,57 +73,41 @@ class MPY(implicit p: Parameters) extends CoreModule {
   //    val valid = io.ctrl =/= default
   //}
 
-  val valid = io.ctrl =/= default
-  val start = Wire(Bool())
-
-  io.busy := start
-
-  rs1Signed := false.B
-  rs2Signed := false.B
-  mostSignificantWord := false.B
-
-  io.prod.valid := false.B
-  io.prod.bits := 0.U
-  start := false.B
-
-  switch(state) {
-    is(sIdle) {
-      io.prod.valid := false.B
-      when(valid) {
-        state := sCompute
-        start := true.B
-      }
-    }
-    is(sCompute) {
-      state := sIdle
-      when(io.ctrl === MUL) {
-        rs1Signed := true.B
-        rs2Signed := true.B
-        mostSignificantWord := false.B
-      }.elsewhen(io.ctrl === MULH) {
-        rs1Signed := true.B
-        rs2Signed := true.B
-        mostSignificantWord := true.B
-      }.elsewhen(io.ctrl === MULHU) {
-        rs1Signed := false.B
-        rs2Signed := false.B
-        mostSignificantWord := true.B
-      }.otherwise {
-        rs1Signed := true.B
-        rs2Signed := false.B
-        mostSignificantWord := true.B
-      }
-      val a = Wire(SInt((xLen + 1).W))
-      val b = Wire(SInt((xLen + 1).W))
-      val p = Wire(SInt(((xLen + 1) * 2 + 1).W))
-
-      a := Cat((rs1Signed && io.A(31)), io.A.asUInt).asSInt
-      b := Cat((rs2Signed && io.B(31)), io.B.asUInt).asSInt
-      p := a * b
-      val prod = Mux(mostSignificantWord, p(63, 32).asSInt, p(31, 0).asSInt).asUInt
-      io.prod.valid := true.B
-      io.prod.bits := prod
-      start := false.B
-    }
+  when(io.ctrl === MUL) {
+    rs1Signed := true.B
+    rs2Signed := true.B
+    mostSignificantWord := false.B
+  }.elsewhen(io.ctrl === MULH) {
+    rs1Signed := true.B
+    rs2Signed := true.B
+    mostSignificantWord := true.B
+  }.elsewhen(io.ctrl === MULHU) {
+    rs1Signed := false.B
+    rs2Signed := false.B
+    mostSignificantWord := true.B
+  }.elsewhen(io.ctrl === MULHSU) {
+    rs1Signed := true.B
+    rs2Signed := false.B
+    mostSignificantWord := true.B
+  }.otherwise {
+    rs1Signed := false.B
+    rs2Signed := false.B
+    mostSignificantWord := false.B
   }
+  val a = Wire(SInt((xLen + 1).W))
+  val b = Wire(SInt((xLen + 1).W))
+  val prodRaw = Wire(SInt(((xLen + 1) * 2 + 1).W))
+
+  io.prod.valid := io.ctrl =/= default
+  when(io.prod.valid) {
+    a := Cat((rs1Signed && io.A(31)), io.A.asUInt).asSInt
+    b := Cat((rs2Signed && io.B(31)), io.B.asUInt).asSInt
+  }.otherwise {
+    a := 0.S
+    b := 0.S
+  }
+  prodRaw := a * b
+  val prod = Mux(mostSignificantWord, prodRaw(63, 32).asSInt, prodRaw(31, 0).asSInt).asUInt
+  io.prod.bits := prod
+  io.busy := false.B
 }
