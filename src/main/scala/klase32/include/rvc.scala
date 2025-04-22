5b6106c844b6b9d197d57a3b8f0b726c64ce3d04
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2025-04-22 21:34:22 +0900

    Update rvc.scala

--- src/main/scala/klase32/include/rvc.scala
+++ src/main/scala/klase32/include/rvc.scala
@@ -1,21 +1,26 @@
-package klase32
+package klase32.include
 
 import chisel3._
 import chisel3.util._
 import chisel3.experimental.BundleLiterals._
-import klase32.config._
-import klase32.param.KLASE32ParamKey
+import klase32.include.config._
+import klase32.include.param.KLASE32ParamKey
+import klase32.include.KLASE32AbstractClass._
 
-import freechips.rocketchip.rocket.{RVCDecoder, ExpandedInstruction}
+import freechips.rocketchip.rocket.RVCDecoder
 
 class RVCExpander(implicit p: Parameters) extends CoreModule {
   val io = IO(new Bundle {
-    val in = UInt(wordsize.W)
-    val out = UInt(wordsize.W)
+    val in = Input(UInt(wordsize.W))
+    val out = Output(UInt(wordsize.W))
   })
 
     if (usingCompressed) {
-      io.out := new RVCDecoder(io.in, xLen = xLen, useAddiForMv = true).decode.bits
+      when(io.in(1,0) =/= 3.U) {
+        io.out := new RVCDecoder(io.in, xLen = xLen, useAddiForMv = false).decode.bits
+      }.otherwise {
+        io.out := io.in
+      }
     } else {
       io.out := io.in
     }
