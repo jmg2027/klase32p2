b78924038aca78327a3e26ce901a5d06a3178520
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2025-04-22 21:33:44 +0900

    Update controlsignal.scala

--- src/main/scala/klase32/include/controlsignal.scala
+++ src/main/scala/klase32/include/controlsignal.scala
@@ -1,11 +1,12 @@
-package klase32
+package klase32.include
 
 import chisel3._
 import chisel3.util._
 import chisel3.experimental.BundleLiterals._
-import klase32.config._
-import klase32.param.KLASE32ParamKey
-import snitch.enums.SnitchEnum
+import klase32.include.config._
+import klase32.include.param.KLASE32ParamKey
+import klase32.include.enums.SnitchEnum
+import klase32.include.Constants
 
 trait ControlEnum extends SnitchEnum
 trait ControlDefaultEnum extends ControlEnum { val default = Value }	// zero default result
@@ -13,88 +14,95 @@ trait ControlDefaultEnableEnum extends ControlDefaultEnum {
   val EN = Value
 }	// default with enable only
 
-object ALUControlIE extends ControlDefaultEnum {
-  val AND = Value	// aluR 00001
-  val OR = Value //       00010
-  val SRL = Value //      00011
-  val XOR = Value //      00100
-  val EQ = Value //       00101
-  val NE = Value //       00110
-  val SLL = Value //      00111
-  val LT = Value	// 8    01000
-  val LTU = Value //      01001
-  val GE = Value //       01010
-  val GEU = Value //      01011
-  val SLT = Value	// 12   01100
-  val SUB = Value //      01101
-  val SRA = Value //      01110
-  val SLTU = Value //     01111
-  val ADD = Value //      10000
+object ControlSignal {
+  object ALUControlIE extends ControlDefaultEnum {
+    val AND = Value // aluR 00001
+    val OR = Value //       00010
+    val SRL = Value //      00011
+    val XOR = Value //      00100
+    val EQ = Value //       00101
+    val NE = Value //       00110
+    val SLL = Value //      00111
+    val LT = Value // 8    01000
+    val LTU = Value //      01001
+    val GE = Value //       01010
+    val GEU = Value //      01011
+    val SLT = Value // 12   01100
+    val SUB = Value //      01101
+    val SRA = Value //      01110
+    val SLTU = Value //     01111
+    val ADD = Value //      10000
 
+  }
+
+
+  object DIVControlIE extends ControlDefaultEnum {
+    val DIV = Value
+    val DIVU = Value
+    val REM = Value
+    val REMU = Value
+  }
+
+  object MPYControlIE extends ControlDefaultEnum {
+    val MUL = Value
+    val MULH = Value
+    val MULHU = Value
+    val MULHSU = Value
+  }
+
+  object FrontendControlIE extends ControlDefaultEnum {
+    val BR = Value
+    val HALT = Value
+    val JAL = Value
+    val MRET = Value
+    val JALR = Value
+    // for debugger
+    val DRET = Value
+  }
+
+  object W0WritebackIE extends ControlDefaultEnableEnum
+  // Write to GPR with no TCM latency, not from lsu
+
+  object IllegalInstIE extends ControlEnum {
+    val normal = Value
+    val illegal = Value
+
+    override val default = this.normal
+  }
+
+  object WFIIE extends ControlDefaultEnableEnum
+
+  object SignedControl extends ControlEnum {
+    val unsigned = Value
+    val signed = Value
+
+    override val default = this.unsigned
+  }
+
+  object LoadControl extends ControlDefaultEnableEnum
+
+  object StoreControl extends ControlDefaultEnableEnum
+
+  object CSRControl extends ControlDefaultEnum {
+    val RW = Value
+    val RS = Value
+    val RC = Value
+  }
+
+  object FenceEnableIE extends ControlDefaultEnableEnum
+
+  object IcacheFlushIE extends ControlDefaultEnableEnum
+
+  object EcallIE extends ControlDefaultEnableEnum
+
+  object EbreakIE extends ControlDefaultEnableEnum
+
+  object MRetIE extends ControlDefaultEnableEnum
+
+  // for debugger
+  object DRetIE extends ControlDefaultEnableEnum
+
+  object RS2NotALUBIE extends ControlDefaultEnableEnum
+
+  object OCDSwbreakMuxIE extends ControlDefaultEnableEnum
 }
-
-
-object DIVControlIE extends ControlDefaultEnum {
-  val DIV = Value
-  val DIVU = Value
-  val REM = Value
-  val REMU = Value
-}
-
-object MPYControlIE extends ControlDefaultEnum {
-  val MUL = Value
-  val MULH = Value
-  val MULHU = Value
-  val MULHSU = Value
-}
-
-object FrontendControlIE extends ControlDefaultEnum {
-  val BR = Value
-  val HALT = Value
-  val JAL = Value
-  val MRET = Value
-  val JALR = Value
-}
-
-object W0WritebackIE extends ControlDefaultEnableEnum
-// Write to GPR with no TCM latency, not from lsu
-
-object IllegalInstIE extends ControlEnum {
-  val normal = Value
-  val illegal = Value
-
-  override val default = this.normal
-}
-
-object WFIIE extends ControlDefaultEnableEnum
-
-object SignedControl extends ControlEnum {
-  val unsigned = Value
-  val signed = Value
-
-  override val default = this.unsigned
-}
-
-object LoadControl extends ControlDefaultEnableEnum
-
-object StoreControl extends ControlDefaultEnableEnum
-
-object CSRControl extends ControlDefaultEnum {
-  val RW = Value
-  val RS = Value
-  val RC = Value
-}
-
-object FenceEnableIE extends ControlDefaultEnableEnum
-
-object IcacheFlushIE extends ControlDefaultEnableEnum
-
-object EcallIE extends ControlDefaultEnableEnum
-
-object EbreakIE extends ControlDefaultEnableEnum
-
-object MRetIE extends ControlDefaultEnableEnum
-
-object RS2NotALUBIE extends ControlDefaultEnableEnum
-
-object OCDSwbreakMuxIE extends ControlDefaultEnableEnum
